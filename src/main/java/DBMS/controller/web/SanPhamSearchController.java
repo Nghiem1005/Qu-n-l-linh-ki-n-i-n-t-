d2cdd3aef6f51d305dc771755d7d3536c7630c69
Nghiem1005 <nghiem100514789123@gmail.com>
2021-11-24 19:23:20 +0700

    addd

--- src/main/java/DBMS/controller/web/SanPhamSearchController.java
+++ src/main/java/DBMS/controller/web/SanPhamSearchController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import DBMS.dao.SanPhamDao;
 import DBMS.model.SanPhamModel;
 
+@SuppressWarnings("serial")
 @WebServlet(urlPatterns = {"/sanpham-search"})
 public class SanPhamSearchController extends HttpServlet {
 	
